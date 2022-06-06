8325adc2de937cab81561446d9453b97e134e497
Lelton Borges <leltondev@gmail.com>
2022-06-06 20:57:54 -0300

    feat: add config url and service autentication

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {BASE} from "../../config/app.config";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  autentica(usuario: string, senha: string): Observable<any>{
+    return this.httpClient.post(`${BASE.urlBase}/user/login`, {
+      userName: usuario,
+      password: senha  
+    })
+  }
 }
