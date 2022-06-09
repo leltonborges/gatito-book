ad7a29dc84876b84f0c91d9b162d7b7cf0c1690e
Lelton Borges <leltondev@gmail.com>
2022-06-09 18:49:03 -0300

    feat: add validatores constumizer

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { NovoUsuarioService } from "./novo-usuario.service";
 import { NovoUsuario } from "./novo-usuario";
+import { minusculoValidator } from "./minusculo.validator";
 
 @Component({
   selector: 'app-novo-usuario',
@@ -28,7 +29,9 @@ export class NovoUsuarioComponent implements OnInit {
           Validators.required,
           Validators.minLength(5)
         ]],
-        userName: [''],
+        userName: ['', [
+          minusculoValidator
+        ]],
         password: ['']
       })
   }
