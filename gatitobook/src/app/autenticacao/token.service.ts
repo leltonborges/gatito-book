df9fe92ecb40271137871e2c833ba7cbc4afa07d
Lelton Borges <leltondev@gmail.com>
2022-06-12 14:06:02 -0300

    feat: add service usuário

--- gatitobook/src/app/autenticacao/token.service.ts
+++ gatitobook/src/app/autenticacao/token.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { keyframes } from "@angular/animations";
+const KEY = 'ACESS_VALID';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
-  constructor() { }
+  constructor() {}
+
+  retornaToken(){
+    return localStorage.getItem(KEY) ?? '';
+  }
+
+  salvaToken(token: string){
+    localStorage.setItem(KEY, token);
+  }
+
+  excluiToken(){
+    localStorage.removeItem(KEY)
+  }
+
+  possuiToken(): Boolean{
+    return !!this.retornaToken();
+  }
 }
