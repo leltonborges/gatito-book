926ef8d2cdff456d36b3e51b03af752f355dc03f
Lelton Borges <leltondev@gmail.com>
2022-06-13 11:41:03 -0300

    feat: add component "cartão"

--- gatitobook/src/app/componentes/cartao/cartao.module.ts
+++ gatitobook/src/app/componentes/cartao/cartao.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartaoComponent } from './cartao.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CartaoComponent
+  ],
   imports: [
     CommonModule
   ]
