6ba5a5d39b481f97dab7bf040f3a2bdcafc82ca9
Lelton Borges <leltondev@gmail.com>
2022-06-13 11:38:44 -0300

    chore: config component "Animal"

--- gatitobook/src/app/animais/animal/animal.component.ts
+++ gatitobook/src/app/animais/animal/animal.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BASE } from "../../../config/app.config";
 
-@Component({
+@Component( {
   selector: 'app-animal',
   templateUrl: './animal.component.html',
-  styleUrls: ['./animal.component.css']
-})
+  styleUrls: [ './animal.component.css' ]
+} )
 export class AnimalComponent implements OnInit {
+  private _url : string = '';
+  private _descricao : string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit() : void {
+  }
+
+  get url() : string {
+    return this._url;
+  }
+
+  @Input()
+  set url( value : string ) {
+    if ( value.startsWith( 'data' ) ) this.url = value;
+    else this.url = `${ BASE.urlBase }/imgs/${ value }`
+  }
+
+  get descricao() : string {
+    return this._descricao;
+  }
+
+  @Input()
+  set descricao( value : string ) {
+    this._descricao = value;
+  }
 }
