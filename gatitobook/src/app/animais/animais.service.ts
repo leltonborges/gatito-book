425e89d621aa2c401d8328c80189e323370f0b2e
Lelton Borges <leltondev@gmail.com>
2022-06-17 13:50:28 -0300

    chore<Animais.service>: add methods: "likePhoto" and "deleteById"

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient, HttpStatusCode } from "@angular/common/http";
+import { catchError, mapTo, Observable, of, throwError } from "rxjs";
 import { Animais, Animal } from "./animal";
 import { TokenService } from "../autenticacao/token.service";
 import { environment } from "../../environments/environment";
@@ -27,6 +27,23 @@ export class AnimaisService {
   findById( id : number ) : Observable<Animal> {
     return this.httpClient.get<Animal>(
       `${ environment.urlBase }/photos/${ id }` )
+  }
+
+  deleteById( id : number ) : Observable<Animal> {
+    return this.httpClient.delete<Animal>( `${ environment.urlBase }/photos/${ id }` )
+  }
+
+  likePhoto( id : number ) : Observable<Boolean> {
+    return this.httpClient.post(
+      `${ environment.urlBase }/photos/${ id }/likes`,
+      {},
+      { observe: 'response' })
+      .pipe(
+        mapTo(true),
+        catchError(err => {
+          return err.status === HttpStatusCode.NotModified ? of(false) : throwError(err)
+        })
+      )
   }
 
 }
