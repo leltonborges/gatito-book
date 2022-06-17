9e77ec5128abdc1ab21f82e05c42d191294b8474
Lelton Borges <leltondev@gmail.com>
2022-06-17 15:10:50 -0300

    chore<ComentarioComponent>: implement template

--- gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { Comentarios } from "./comentarios";
 
-@Component({
+@Component( {
   selector: 'app-comentarios',
   templateUrl: './comentarios.component.html',
-  styleUrls: ['./comentarios.component.css']
-})
+  styleUrls: [ './comentarios.component.css' ]
+} )
 export class ComentariosComponent implements OnInit {
 
-  constructor() { }
+  private _comentarios$ !: Observable<Comentarios>;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit() : void {
+  }
+
+
+  get comentarios$() : Observable<Comentarios> {
+    return this._comentarios$;
+  }
+
+  set comentarios$( value : Observable<Comentarios> ) {
+    this._comentarios$ = value;
+  }
 }
