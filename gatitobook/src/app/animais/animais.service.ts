9b6c39a2e34bfdcd5efbfc48b27b5d5c9f294303
Lelton Borges <leltondev@gmail.com>
2022-06-17 11:57:33 -0300

    refactory: Registry interceptor
    - Remove configuration to "Animais.service.ts"
    - Add provider to "Autenticacao.module.ts"
    - Import module "Autenticacao.modute" in "App.module.ts"

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Animais, Animal } from "./animal";
 import { TokenService } from "../autenticacao/token.service";
@@ -18,28 +18,15 @@ export class AnimaisService {
 
   listDoUsuario( nomeUsuario : String ) : Observable<Animais> {
     if ( nomeUsuario ) {
-      const token = this.getToken();
-      const headers = this.addHeader( token, 'x-access-token' );
       return this.httpClient.get<Animais>(
-        `${ environment.urlBase }/${ nomeUsuario }/photos`,
-        { headers } )
+        `${ environment.urlBase }/${ nomeUsuario }/photos` )
     }
     return new Observable<Animais>()
   }
 
   findById( id : number ) : Observable<Animal> {
-    const token = this.getToken();
-    const headers = this.addHeader( token, 'x-access-token' );
     return this.httpClient.get<Animal>(
-      `${ environment.urlBase }/photos/${ id }`,
-      { headers } )
+      `${ environment.urlBase }/photos/${ id }` )
   }
 
-  private addHeader( token : string, nameHeader : string ) {
-    return new HttpHeaders().append( nameHeader, token )
-  }
-
-  private getToken() {
-    return this.tokenService.getToken()
-  }
 }
