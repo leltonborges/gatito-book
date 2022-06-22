ed17f228db02549952a1b5f7015d57733f1e4f7d
Lelton Borges <leltondev@gmail.com>
2022-06-22 20:46:34 -0300

    feat(menu): Implements component and template

--- gatitobook/src/app/componentes/menu/menu.component.ts
+++ gatitobook/src/app/componentes/menu/menu.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: [ './menu.component.css' ]
 })
 export class MenuComponent implements OnInit {
+  private _mostraMenu = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit() : void {
+  }
+
+  abreMenu() {
+    this._mostraMenu = !this._mostraMenu;
+  }
+
+  get mostraMenu() : boolean {
+    return this._mostraMenu;
+  }
+
+  set mostraMenu(value : boolean) {
+    this._mostraMenu = value;
+  }
 }
