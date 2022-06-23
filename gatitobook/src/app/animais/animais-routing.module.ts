72ee2ae422c6667d3a14c9349b0d8232885e46fe
Lelton Borges <leltondev@gmail.com>
2022-06-22 21:10:50 -0300

    chore(AnimaisRoutingModule): add routing to component "NovoAnimalComponent"

--- gatitobook/src/app/animais/animais-routing.module.ts
+++ gatitobook/src/app/animais/animais-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListaAnimaisComponent } from "./lista-animais/lista-animais.component";
 import { DetalheAnimalComponent } from "./detalhe-animal/detalhe-animal.component";
 import { ListaAnimaisResolver } from "./lista-animais/lista-animais.resolver";
+import { NovoAnimalComponent } from "./novo-animal/novo-animal.component";
 
 const routes : Routes = [
   {
@@ -15,6 +16,10 @@ const routes : Routes = [
   {
     path: ':animaId',
     component: DetalheAnimalComponent
+  },
+  {
+    path: 'novo',
+    component: NovoAnimalComponent
   }
 ];
 
