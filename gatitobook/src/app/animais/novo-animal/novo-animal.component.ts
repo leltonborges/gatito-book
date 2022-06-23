df2b7d7b0ff5a792136534f814a31489b411359f
Lelton Borges <leltondev@gmail.com>
2022-06-23 14:28:34 -0300

    feat(novoAnimal): implements template and component

--- gatitobook/src/app/animais/novo-animal/novo-animal.component.ts
+++ gatitobook/src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-novo-animal',
   templateUrl: './novo-animal.component.html',
-  styleUrls: ['./novo-animal.component.css']
+  styleUrls: [ './novo-animal.component.css' ]
 })
 export class NovoAnimalComponent implements OnInit {
+  private _formAnimal ! : FormGroup;
+  private _file! : File;
+  private _preview ! : string;
+  private _percentualConcluido = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit() : void {
+  }
+
+  upload() {
+  }
+
+  gravaArquivo(arquivo : any) {
+  }
+
+  get formAnimal() : FormGroup {
+    return this._formAnimal;
+  }
+
+  set formAnimal(value : FormGroup) {
+    this._formAnimal = value;
+  }
+
+  get file() : File {
+    return this._file;
+  }
+
+  set file(value : File) {
+    this._file = value;
+  }
+
+  get preview() : string {
+    return this._preview;
+  }
+
+  set preview(value : string) {
+    this._preview = value;
+  }
+
+  get percentualConcluido() : number {
+    return this._percentualConcluido;
+  }
+
+  set percentualConcluido(value : number) {
+    this._percentualConcluido = value;
+  }
 }
